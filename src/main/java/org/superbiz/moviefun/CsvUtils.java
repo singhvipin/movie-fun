8f070e12eecb9d0b699e9f6f21659a1449094b60
pal <you@example.com>
2018-12-12 11:53:04 +0530

    file persistence 1

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -12,19 +12,16 @@ import java.util.Scanner;
 
 public class CsvUtils {
 
-    public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+    public static String readFile(String path) throws FileNotFoundException {
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        //Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+        Scanner scanner = new Scanner(CsvUtils.class.getClassLoader().getResourceAsStream(path)).useDelimiter("\\A");
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
         }
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
